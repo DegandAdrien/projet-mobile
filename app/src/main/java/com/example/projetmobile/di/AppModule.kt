e3037291953c0f568fa62bc24c8351ea43753889
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2025-03-31 09:58:13 +0200

    add repos to module

--- app/src/main/java/com/example/projetmobile/di/AppModule.kt
+++ app/src/main/java/com/example/projetmobile/di/AppModule.kt
@@ -5,6 +5,12 @@ import android.content.Context
 import androidx.room.Room
 import com.example.projetmobile.data.AppDatabase
 import com.example.projetmobile.data.LicensePlateDao
+import com.example.projetmobile.repository.LicensePlateRepository
+import com.example.projetmobile.repository.LicensePlateRepositoryImpl
+import com.example.projetmobile.repository.LocationRepository
+import com.example.projetmobile.repository.LocationRepositoryImpl
+import com.example.projetmobile.repository.TextRecognitionRepository
+import com.example.projetmobile.repository.TextRecognitionRepositoryImpl
 
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -37,8 +43,29 @@ object AppModule {
 
     @Provides
     @Singleton
+    fun provideLicensePlateRepository(dao: LicensePlateDao): LicensePlateRepository {
+        return LicensePlateRepositoryImpl(dao)
+    }
+
+    @Provides
+    @Singleton
     fun provideFusedLocationClient(@ApplicationContext context: Context): FusedLocationProviderClient {
         return LocationServices.getFusedLocationProviderClient(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocationRepository(
+        fusedLocationClient: FusedLocationProviderClient,
+        @ApplicationContext context: Context
+    ): LocationRepository {
+        return LocationRepositoryImpl(fusedLocationClient, context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTextRecognitionRepository(): TextRecognitionRepository {
+        return TextRecognitionRepositoryImpl()
     }
 }
 

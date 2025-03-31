67666608d77529a4c50d832c42dc2419d89a6dda
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2025-03-31 09:00:26 +0200

    Début scan

--- app/src/main/java/com/example/projetmobile/MainActivity.kt
+++ app/src/main/java/com/example/projetmobile/MainActivity.kt
@@ -16,32 +16,8 @@ import com.example.projetmobile.ui.theme.ProjetMobileTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            ProjetMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjetMobileTheme {
-        Greeting("Android")
     }
 }
